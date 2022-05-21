996037cf5717cd8792c00e8c33067351b178eecc
HarjasSodhi <harjassodhi@yahoo.in>
2022-05-21 17:46:16 +0530

    responsive 1024px

--- JS/caraousel.js
+++ JS/caraousel.js
@@ -29,58 +29,13 @@
 })();
 
 
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 50,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
-
-// (function handleShifts() {
-//     let leftBtn = document.querySelector('.caraousel .left')
-//     let rightBtn = document.querySelector('.caraousel .right')
-//     let boxes = document.querySelectorAll('.tray .content')
-//     let leftIdx = 0;
-//     let rightIdx = 2;
-//     function handleLeftClick(e) {
-//     boxes.forEach(el => {
-//         let rect=el.getBoundingClientRect()
-//         let num=rect.left-(rect.right-rect.left)
-//         el.style.transform = `translateX(${num}px)`
-//     })
-//     return;
-//     console.log(boxes)
-//         console.log(leftIdx, rightIdx)
-//         if (leftIdx == 0) return;
-//         leftIdx--;
-//         rightIdx--;
-//         boxes[leftIdx].scrollIntoView({
-//             behavior: "smooth",
-//             inline: "nearest",
-//             block: "end"
-//         })
-//     }
-
-//     function handleRightClick(e) {
-//         boxes.forEach(el => {
-//             let rect=el.getBoundingClientRect()
-//             let num=rect.left+(rect.right-rect.left)
-//             el.style.transform = `translateX(${num}px)`
-//         })    
-//         return ;
-//         console.log(leftIdx, rightIdx)
-//         if (rightIdx == boxes.length - 1) return;
-//         rightIdx++;
-//         leftIdx++;
-//         boxes[rightIdx].scrollIntoView({
-//             behavior: "smooth",
-//             inline: "nearest",
-//             block: "end"
-//         })
-//     }
-
-//     leftBtn.addEventListener('click', handleLeftClick)
-//     rightBtn.addEventListener('click', handleRightClick)
-// })();
+(function handleCaraousel() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+    })
+})();
